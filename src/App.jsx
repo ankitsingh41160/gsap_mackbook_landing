d2d2424d9b75c36df9faee47d0689c167070018a
Ankit singh <ankitsinghrajput41160@gmail.com>
2026-04-25 19:41:42 +0000

    Render all sections in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Features from "./components/Features.jsx";
+import Performance from "./components/Performance.jsx";
+import Showcase from "./components/Showcase.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero />
+            <Highlights />
+            <Features />
+            <Performance />
+            <Showcase />
+            <Footer />
         </main>
     )
 }
+
 export default App
